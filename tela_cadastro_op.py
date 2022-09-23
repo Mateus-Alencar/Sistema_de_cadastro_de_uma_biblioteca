53d59da577d8934e0afded057181521a5b673772
Mateus-Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2022-09-23 18:46:38 -0300

    mudanças na tela de cadastros

--- tela_cadastro_op.py
+++ tela_cadastro_op.py
@@ -15,11 +15,34 @@ def icon_comeco(self, usu):
         label = Label(self.frame, text='Cliente', font='Arial 20 bold', bg='#d9d9d9', fg='black')
         label.pack()
 
+    self.frame2 = Frame(self.window).pack()
+    button_cadastar = Button(self.frame2, text=('Cadastrar'), font='Arial 16 bold', width=10, height=2, bg='#00c2cb', fg='black')
+    button_cadastar.place(x=180, y= 660)
+    button_login = Button(self.frame2, text=('Login'), font='Arial 16 bold', width=10, height=2, bg='#00c2cb', fg='black')
+    button_login.place(x=180, y= 740)
 
 def tela_cadastroCliente(self, usu):
     icon_comeco(self, usu)
+    nome = Label(self.frame2, text='Nome: ',bg="#d9d9d9",font=('Ivy 15'))
+    nome.place(x=35,y=300)
+    entry_nome = Entry(self.frame2, width=25, justify='left',font='Arial 15 bold', relief='solid')
+    entry_nome.place(x=130,y=300)
+
+    email = Label(self.frame2, text='E-mail: ',bg="#d9d9d9",font=('Ivy 15'))
+    email.place(x=35,y=350)
+    entry_email = Entry(self.frame2, width=25, justify='left',font='Arial 15 bold', relief='solid')
+    entry_email.place(x=130,y=350)
 
 
 def tela_cadastroFuncionario(self, usu):
     icon_comeco(self, usu)
+    nome = Label(self.frame2, text='Nome: ',bg="#d9d9d9",font=('Ivy 15'))
+    nome.place(x=35,y=300)
+    entry_nome = Entry(self.frame2, width=25, justify='left',font='Arial 15 bold', relief='solid')
+    entry_nome.place(x=130,y=300)
+
+    email = Label(self.frame2, text='E-mail: ',bg="#d9d9d9",font=('Ivy 15'))
+    email.place(x=35,y=350)
+    entry_email = Entry(self.frame2, width=25, justify='left',font='Arial 15 bold', relief='solid')
+    entry_email.place(x=130,y=350)
     
